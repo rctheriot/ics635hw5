ddefa49af5a7bc07e5c7fda0cb9b4f8e19b9bc9f
Ryan Theriot <ryanctheriot@gmail.com>
2018-04-17 20:08:01 -1000

    Removed train/test

--- hw5.py
+++ hw5.py
@@ -7,56 +7,41 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import operator
 
+centroids = 4
 
-features = 21
+X, y = make_blobs(n_features=2, centers=centroids, cluster_std=0.5)
 
-X, y = make_blobs(n_features=features, centers=features, cluster_std=10)
-test = []
-test_labels = []
+kmeans = KMeans(n_clusters=centroids, random_state=0).fit(X)
+prediction = kmeans.predict(X)
+data_length = len(X)
 
-data_len = len(X)
-
-for i in range(1,int(data_len * 0.80)):
-  randomNum = randint(0, len(X) - 1)
-  test.append(X[randomNum])
-  test_labels.append(y[randomNum])
-  X = np.delete(X, randomNum, 0)
-  y = np.delete(y, randomNum, 0)
-
-test = np.array(test)
-test_labels = np.array(test_labels)
-
-kmeans = KMeans(n_clusters=features, random_state=0).fit(X)
-prediction = kmeans.predict(test)
-test_len = len(test)
-
-num_labels = 0
-num_corr = 0
 label_array = []
 prediction_array = []
 prediction_array2 = []
 matches = []
 
-for i in range(0, features):
+for i in range(0, centroids):
   label_array.append([])
   prediction_array.append([])
   prediction_array2.append([])
   matches.append([])
 
-for i in range(0, len(test_labels)):
-  label_array[test_labels[i]].append(i)
+for i in range(0, len(y)):
+  label_array[y[i]].append(i)
 for i in range(0, len(prediction)):
   prediction_array[prediction[i]].append(i)
 
-for i in range(0, features):
-  for j in range(0, features):
+for i in range(0, centroids):
+  for j in range(0, centroids):
     matches[i].append(len(set(label_array[i]) & set(prediction_array[j])))
 
-for i in range(0, features):
+for i in range(0, centroids):
   index, value = max(enumerate(matches[i]), key=operator.itemgetter(1))
   prediction_array2[i] = prediction_array[index]
 
-for i in range(0, features):
+num_labels = 0
+num_corr = 0
+for i in range(0, centroids):
   num_labels = num_labels + len(prediction_array2[i])
   num_corr = num_corr + len(set(label_array[i]) & set(prediction_array2[i]))
 
